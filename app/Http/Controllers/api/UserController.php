97bceadb8231b1a22a3d8f837356c5e1b632a9e6
Hammam Firdaus <h.firdaus@aryo.jp>
2021-01-05 05:49:48 +0700

    create auth passport

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed!',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Success!',
+            'data'    => $user,
+            'token'   => $user->createToken('authToken')->accessToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $removeToken = $request->user()->tokens()->delete();
+
+        if($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Success!',
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', 'api\UserController@login');
+Route::post('/logout', 'api\UserController@logout')->middleware('auth:api');
